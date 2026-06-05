38a898e6b29b99f3a093c8670247c66274727eb2
hosam zaki <dolsika5762@gmail.com>
2026-06-05 20:19:17 +0300

    complete account guide

--- src/views/account_guides/validation/validationRequest.js
+++ src/views/account_guides/validation/validationRequest.js
@@ -9,21 +9,21 @@ export default {
       this.errors = {}
 
       if (!form.company_id?.trim()) {
-        this.errors.company_id = this.$t('categories.validation.companyIdRequired')
+        this.errors.company_id = this.$t('accountGuides.validation.companyIdRequired')
       }
 
       if (!form.account_guide_type_id?.trim()) {
         this.errors.account_guide_type_id = this.$t(
-          'categories.validation.accountGuideTypeIdRequired'
+          'accountGuides.validation.accountGuideTypeIdRequired'
         )
       }
 
       if (!form.name?.trim()) {
-        this.errors.name = this.$t('categories.validation.nameRequired')
+        this.errors.name = this.$t('accountGuides.validation.nameRequired')
       }
 
       if (!form.name_ar?.trim()) {
-        this.errors.name_ar = this.$t('categories.validation.nameArRequired')
+        this.errors.name_ar = this.$t('accountGuides.validation.nameArRequired')
       }
 
       return Object.keys(this.errors).length === 0
@@ -33,11 +33,11 @@ export default {
       this.errors = {}
 
       if (!form.name?.trim()) {
-        this.errors.name = this.$t('categories.validation.nameRequired')
+        this.errors.name = this.$t('accountGuides.validation.nameRequired')
       }
 
       if (!form.name_ar?.trim()) {
-        this.errors.name_ar = this.$t('categories.validation.nameArRequired')
+        this.errors.name_ar = this.$t('accountGuides.validation.nameArRequired')
       }
 
       return Object.keys(this.errors).length === 0
